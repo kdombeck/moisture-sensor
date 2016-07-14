525caf6fa0bb0f5eb59dd0e54933c8b562db909a
Ken Dombeck <ken.dombeck@target.com>
2016-07-13 22:37:24 -0500

    Added Wifi to the receiver and sending it up to Adafruit.io

--- receiverLoraBreakout/receiverLoraBreakout.ino
+++ receiverLoraBreakout/receiverLoraBreakout.ino
@@ -1,7 +1,12 @@
 #include <SPI.h>
 #include <RH_RF95.h>
 #include <RHSoftwareSPI.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+#include <Adafruit_WINC1500.h>
+#include "secrets.h"
 
+// LoRa breakout setup
 #define RFM95_SCK 13
 #define RFM95_MOSI 11
 #define RFM95_MISO 12
@@ -10,22 +15,50 @@ RHSoftwareSPI loraSpi;
 #define RFM95_CS 10
 #define RFM95_RST 5
 #define RFM95_INT 6
-
-// Change to 434.0 or other frequency, must match RX's freq!
 #define RF95_FREQ 915.0
 
-// Singleton instance of the radio driver
 RH_RF95 rf95(RFM95_CS, RFM95_INT, loraSpi);
+
+// MQTT and WIFI setup
+#define WINC_CS   8
+#define WINC_IRQ  7
+#define WINC_RST  4
+#define WINC_EN   2
+
+Adafruit_WINC1500 WiFi(WINC_CS, WINC_IRQ, WINC_RST);
+
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883
+
+Adafruit_WINC1500Client client;
+
+int status = WL_IDLE_STATUS;
+
+// Store the MQTT server, client ID, username, and password in flash memory.
+// This is required for using the Adafruit MQTT library.
+const char MQTT_SERVER[] PROGMEM    = AIO_SERVER;
+// Set a unique MQTT client ID using the AIO key + the date and time the sketch
+// was compiled (so this should be unique across multiple devices for a user,
+// alternatively you can manually set this to a GUID or other random value).
+const char MQTT_CLIENTID[] PROGMEM  = "WINC1500-client";
+const char MQTT_USERNAME[] PROGMEM  = AIO_USERNAME;
+const char MQTT_PASSWORD[] PROGMEM  = AIO_KEY;
+
+Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, AIO_SERVERPORT, MQTT_CLIENTID, MQTT_USERNAME, MQTT_PASSWORD);
+
+// Setup a feed called 'moisture' for publishing.
+// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
+const char MOISTURE_FEED[] PROGMEM = AIO_USERNAME "/feeds/moisture";
+Adafruit_MQTT_Publish moisture = Adafruit_MQTT_Publish(&mqtt, MOISTURE_FEED);
 
 void setup() {
 //  while ( ! Serial ) { delay( 10 ); } // wait for serial connection
   Serial.begin(115200);
 
+  Serial.println("LoRa setup");
   loraSpi.setPins(RFM95_MISO, RFM95_MOSI, RFM95_SCK);
   pinMode(RFM95_RST, OUTPUT);
   digitalWrite(RFM95_RST, HIGH);
-
-  Serial.println("Arduino LoRa TX Test!");
 
   // manual reset
   digitalWrite(RFM95_RST, LOW);
@@ -42,7 +75,7 @@ void setup() {
   // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
   if (!rf95.setFrequency(RF95_FREQ)) {
     Serial.println("setFrequency failed");
-    while (1);
+    while (true);
   }
   Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
 
@@ -52,9 +85,25 @@ void setup() {
   // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
   // you can set transmitter powers from 5 to 23 dBm:
   rf95.setTxPower(23, false);
+
+  Serial.println("WIFI setup");
+  pinMode(WINC_EN, OUTPUT);
+  digitalWrite(WINC_EN, HIGH);
+
+  // check for the presence of the breakout
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WINC1500 not present");
+    while (true);
+  }
+  Serial.println("ATWINC OK!");
 }
 
 void loop() {
+  // Ensure the connection to the MQTT server is alive (this will make the first
+  // connection and automatically reconnect when disconnected).  See the MQTT_connect
+  // function definition further below.
+  MQTT_connect();
+
   if (rf95.available()) {
     // Should be a message for us now
     uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
@@ -67,6 +116,12 @@ void loop() {
       Serial.print("RSSI: ");
       Serial.println(rf95.lastRssi(), DEC);
 
+      if (moisture.publish((char*)buf)) {
+        Serial.println("OK!");
+      } else {
+        Serial.println("Failed");
+      }
+
       // Send a reply
       uint8_t data[] = "And hello back to you";
       rf95.send(data, sizeof(data));
@@ -76,5 +131,40 @@ void loop() {
       Serial.println("Receive failed");
     }
   }
+}
+
+// Function to connect and reconnect as necessary to the MQTT server.
+// Should be called in the loop function and it will take care if connecting.
+void MQTT_connect() {
+  int8_t ret;
+
+  // attempt to connect to Wifi network:
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: "); Serial.println(WIFI_SSID);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+    // wait 10 seconds for connection:
+    uint8_t timeout = 10;
+    while (timeout && (WiFi.status() != WL_CONNECTED)) {
+      timeout--;
+      delay(1000);
+    }
+  }
+
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Retrying MQTT connection in 5 seconds...");
+       mqtt.disconnect();
+       delay(5000);  // wait 5 seconds
+  }
+  Serial.println("MQTT Connected!");
 }
 
