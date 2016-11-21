dcae36e5259b8b84b42d87f49f74f20908d9af96
Ken Dombeck <kdombeck@gmail.com>
2016-11-20 22:03:44 -0600

    Added proper logging

--- raspberrypi/usr/local/bin/sensorservice/sensorservice.py
+++ raspberrypi/usr/local/bin/sensorservice/sensorservice.py
@@ -3,30 +3,32 @@
 import serial
 import string
 import requests
+import logging
 
 MESSAGE_PREFIX = "Got:  "
 
 class SensorService:
+    logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
 
     def processMessage(self, message):
         message = message.strip()
         if self.isValidMessage(message):
-            print('processing message: ' + message)
+            logging.info('processing message: ' + message)
             self.insertDB(message[len(MESSAGE_PREFIX):])
             return True
         else:
-            print('not processing message: ' + message)
+            logging.debug('not processing message: ' + message)
             return False
 
     def isValidMessage(self, message):
         if message.startswith(MESSAGE_PREFIX) == False:
-            print('message doesn\'t start with "' + MESSAGE_PREFIX + '": ' + message)
+            logging.debug('message doesn\'t start with "' + MESSAGE_PREFIX + '": ' + message)
             return False
         elif self.isAscii(message) == False:
-            print('message is not ASCII: ' + message)
+            logging.warn('message is not ASCII: ' + message)
             return False
         elif message.count(',') <= 2:
-            print('message does not have enough comma\'s: ' + message)
+            logging.warn('message does not have enough comma\'s: ' + message)
             return False
         else:
             return True
@@ -40,7 +42,7 @@ class SensorService:
     def insertDB(self, message):
         r = requests.post("http://localhost:8086/write?db=sensordb", data=self.createBodyForDB(message))
         if r.status_code != 204:
-            print('Failed to send payload to influxdb: ' + str(r.status_code) + ' ' + r.text)
+            logging.error('Failed to send payload to influxdb: ' + str(r.status_code) + ' ' + r.text)
 
     def createBodyForDB(self, message):
         host, measure, measurements = message.split(',', 2)
