2655e03ab1ff5adc30378eee78c37862bc061583
Ken Dombeck <kdombeck@gmail.com>
2016-11-24 09:35:17 -0600

    Create datasource for InfluxDB in Grafana

--- raspberrypi/install.sh
+++ raspberrypi/install.sh
@@ -41,4 +41,5 @@ systemctl daemon-reload
 systemctl enable grafana-server
 systemctl start grafana-server
 
+curl -X POST --user admin:admin localhost:3000/api/datasources -H 'Content-Type: application/json' -d '{"name":"Sensor Database","type":"influxdb","access":"proxy","url":"http://localhost:8086","database":"sensordb"}'
 echo "finished installing"
