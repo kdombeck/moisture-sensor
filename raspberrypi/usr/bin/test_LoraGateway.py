1ab39d0310a25072b51b8d1d80bc4e112eb294a7
Ken Dombeck <kdombeck@gmail.com>
2016-11-24 11:00:49 -0600

    Removed need for creating service variable

--- raspberrypi/usr/bin/test_LoraGateway.py
+++ raspberrypi/usr/bin/test_LoraGateway.py
@@ -1,24 +1,21 @@
 # coding: utf-8
 
 import unittest
-import LoraGateway
+from LoraGateway import LoraGateway
 
 class TestLoraGateway(unittest.TestCase):
-    def setUp(self):
-        self.service = LoraGateway.LoraGateway()
-
     # def test_process_message(self):
         # need to mock 'requests' for this to work
-        # self.assertTrue(self.service.process_message('Got:  32B,sensor,moisture1=259,moisture2=248,moisture3=190,battery=0.00'))
+        # self.assertTrue(LoraGateway().process_message('Got:  32B,sensor,moisture1=259,moisture2=248,moisture3=190,battery=0.00'))
 
     def test_is_valid_message(self):
-        self.assertFalse(self.service.is_valid_message('blah'))
-        self.assertFalse(self.service.is_valid_message('Got:  €⁄€‹›ﬁﬂ‡°·‚'))
-        self.assertFalse(self.service.is_valid_message('Got:  foo,bar'))
-        self.assertTrue(self.service.is_valid_message('Got:  32B,sensor,moisture1=259,moisture2=248,moisture3=190,battery=0.00'))
+        self.assertFalse(LoraGateway().is_valid_message('blah'))
+        self.assertFalse(LoraGateway().is_valid_message('Got:  €⁄€‹›ﬁﬂ‡°·‚'))
+        self.assertFalse(LoraGateway().is_valid_message('Got:  foo,bar'))
+        self.assertTrue(LoraGateway().is_valid_message('Got:  32B,sensor,moisture1=259,moisture2=248,moisture3=190,battery=0.00'))
 
     def test_create_body_for_db(self):
-        self.assertEquals(self.service.create_body_for_db('32B,sensor,moisture1=259,moisture2=248'), 'sensor,host=32B moisture1=259,moisture2=248')
+        self.assertEquals(LoraGateway().create_body_for_db('32B,sensor,moisture1=259,moisture2=248'), 'sensor,host=32B moisture1=259,moisture2=248')
 
 if __name__ == '__main__':
     unittest.main()
