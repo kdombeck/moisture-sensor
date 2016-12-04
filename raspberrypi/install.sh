3e7a6c7a55f23d674327a32434b2ee162520a845
Ken Dombeck <kdombeck@gmail.com>
2016-12-03 21:09:57 -0600

    Fixed mis spelling of Grafana file

--- raspberrypi/install.sh
+++ raspberrypi/install.sh
@@ -42,6 +42,6 @@ systemctl enable grafana-server
 systemctl start grafana-server
 
 curl -X POST --user admin:admin localhost:3000/api/datasources -H 'Content-Type: application/json' -d '{"name":"Sensor Database","type":"influxdb","access":"proxy","url":"http://localhost:8086","database":"sensordb"}'
-curl -X POST --user admin:admin localhost:3000/api/dashboards/db -H 'Content-Type: application/json' -d @grafaDashboard.json
+curl -X POST --user admin:admin localhost:3000/api/dashboards/db -H 'Content-Type: application/json' -d @grafanaDashboard.json
 
 echo "finished installing"
