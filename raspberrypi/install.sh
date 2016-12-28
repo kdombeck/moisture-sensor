1802c77bd3e45b3df637a551306f25baf55a56ad
Ken Dombeck <kdombeck@gmail.com>
2016-12-27 21:43:38 -0600

    Upgraded Grafana to version 4.0.2. This version gives alerts.

--- raspberrypi/install.sh
+++ raspberrypi/install.sh
@@ -4,7 +4,6 @@ apt-get update && apt-get -y upgrade
 apt-get install -y apt-transport-https
 
 echo "!!! Installing Python !!!!"
-# apt-get install -y python3 python3-pip
 pip install pyserial
 
 echo "!!!! Installing InfluxDB !!!!"
@@ -14,7 +13,7 @@ test $VERSION_ID = "7" && echo "deb https://repos.influxdata.com/debian wheezy s
 test $VERSION_ID = "8" && echo "deb https://repos.influxdata.com/debian jessie stable" | tee /etc/apt/sources.list.d/influxdb.list
 
 apt-get update && apt-get install -y influxdb
-systemctl start influxdb
+systemctl restart influxdb
 
 curl -i -XPOST http://localhost:8086/query --data-urlencode "q=CREATE DATABASE sensordb"
 
@@ -24,22 +23,16 @@ cp lib/systemd/system/loragateway.service /lib/systemd/system/loragateway.servic
 
 systemctl daemon-reload
 systemctl enable loragateway.service
-systemctl start loragateway
+systemctl restart loragateway
 
 echo "!!!! Installing Grafana !!!!"
-curl -OL https://github.com/fg2it/grafana-on-raspberry/releases/download/v3.1.1-wheezy-jessie/grafana_3.1.1-1472506485_armhf.deb
+curl -OL https://github.com/fg2it/grafana-on-raspberry/releases/download/v4.0.2/grafana_4.0.2-1481228559_armhf.deb
 apt-get install -y adduser libfontconfig
-dpkg -i grafana_3.1.1-1472506485_armhf.deb
-
-# iptables -I INPUT -p tcp --dport 80 -j ACCEPT
-# iptables -I INPUT -p tcp --dport 3000 -j ACCEPT
-# iptables -t nat -A PREROUTING -p tcp -m tcp --dport 80 -j REDIRECT --to-port 3000
-# 
-# iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 3000
+dpkg -i grafana_4.0.2-1481228559_armhf.deb
 
 systemctl daemon-reload
 systemctl enable grafana-server
-systemctl start grafana-server
+systemctl restart grafana-server
 
 curl -X POST --user admin:admin localhost:3000/api/datasources -H 'Content-Type: application/json' -d '{"name":"Sensor Database","type":"influxdb","access":"proxy","url":"http://localhost:8086","database":"sensordb"}'
 curl -X POST --user admin:admin localhost:3000/api/dashboards/db -H 'Content-Type: application/json' -d @grafanaDashboard.json
